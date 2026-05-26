0e10296e3b177d55254b520da85a3a21763bd61f
Mavine <mavinemalin@gmail.com>
2026-05-26 11:16:01 +0300

    atom ear atom surprise particular

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,3 +1,7 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 import {
   FIREBASE_API_KEY,
   FIREBASE_AUTH_DOMAIN,
